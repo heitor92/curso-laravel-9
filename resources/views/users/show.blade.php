3ab87582f0ecb5789fe8d6bb512e9e1b967a4748
Heitor de Souza <heisou06@gmail.com>
2022-08-25 17:38:47 -0400

    Trabalhando com templates Blade no Laravel

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem do usuário')
+
+@section('content')
 <h1>Listagem do usuários {{ $user->name }}</h1>
 
 <ul>
     <li>{{ $user->name }}</li>
     <li>{{ $user->email }}</li>
-</ul>
+</ul>    
+@endsection
